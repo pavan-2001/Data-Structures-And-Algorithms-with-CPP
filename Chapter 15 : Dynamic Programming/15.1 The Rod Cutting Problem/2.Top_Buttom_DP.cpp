8d4f6efab7a99051315ad150d64707a368b694b4
pavan kumar <60856229+pavan-2001@users.noreply.github.com>
2021-01-16 00:45:24 +0530

    Update 2.Top_Buttom_DP.cpp

--- Chapter 15 : Dynamic Programming/15.1 The Rod Cutting Problem/2.Top_Buttom_DP.cpp
+++ Chapter 15 : Dynamic Programming/15.1 The Rod Cutting Problem/2.Top_Buttom_DP.cpp
@@ -6,19 +6,24 @@ using namespace std;
 
 int n=10;
 vector<int> revenue(n+1,-1e9);
-
-int Top_Buttom_Cut_Rod(vector<int> &price,int size)
+vector<int> s(n+1,0);
+int Extended_Top_Buttom_Cut_Rod(vector<int> &price,int size)
 {
     if(revenue[size]>=0)
     return revenue[size];
-    int q;
+    int q,temp;
     if(size==0)
     q=0;
     else
     {   q=-1e9;
         for(int i=1;i<=size;i++)
         {
-            q=max(q,price[i]+Top_Buttom_Cut_Rod(price,size-i));
+            temp=(q,price[i]+Extended_Top_Buttom_Cut_Rod(price,size-i));
+            if(temp>q)
+            {
+                q=temp;
+                s[size]=i;
+            }
         }
     }
     revenue[size]=q;
@@ -27,8 +32,14 @@ int Top_Buttom_Cut_Rod(vector<int> &price,int size)
 
 void Print_Cut_Rod_Solution(vector<int> &price,int size)
 {
-    int result=Top_Buttom_Cut_Rod(price,size);
+    int result=Extended_Top_Buttom_Cut_Rod(price,size);
     cout<<"The Highest Revenue Possible is : "<<result<<endl;
+    cout<<"The possible cutting is : ";
+    while(size>0)
+    {
+        cout<<s[size]<<' ';
+        size-=s[size];
+    }
 }
 
 int main()
@@ -40,4 +51,5 @@ int main()
 /*
 Output : 
 The Highest Revenue Possible is : 30
+The possible cutting is : 10   
 */
